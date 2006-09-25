813bb84b478334114a05607b610911ee692832fe
rk-hannover <rk-hannover@e06bf135-151a-0410-b1a0-f07b4d66640d>
2006-09-25 19:14:18 +0000

    version 1

---
+++ tt-universal/import2db.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+
+do "parameters.inc.pl";
+
+print $STAT_DB . "\n";
+print $STAT_DB_USER . "\n";
+print $STAT_DB_PASS . "\n";
+
+print $LOG_TABLE . "\n";
+
+print $STAT_STARTDIR . "\n";
+print $STAT_SAVEDIR . "\n";
+
+print $FOC_IMPORTDIR . "\n";
+print $FOC_TABLENAME . "\n";
