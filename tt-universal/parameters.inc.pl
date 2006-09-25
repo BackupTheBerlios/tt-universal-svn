6ef3a644b9a0a996d6a855b0e8f0b9cc47d1b42e
rk-hannover <rk-hannover@e06bf135-151a-0410-b1a0-f07b4d66640d>
2006-09-25 19:14:44 +0000

    parameter init

---
+++ tt-universal/parameters.inc.pl
@@ -0,0 +1,11 @@
+my $STAT_DB = 'spicers_stat'; #Name der Stat Datenbank
+my $STAT_DB_USER = 'root';    #Username für zugriff auf DB
+my $STAT_DB_PASS = '';        #passwort für zugriff auf DB
+
+my $LOG_TABLE = 'import_log'; #name der logging tabelle
+
+my $STAT_STARTDIR = 'G:/win_data/projekte/spicers';     #wo liegen die daten der anwendung
+my $STAT_SAVEDIR = 'save';                              #wo werden die eingelesenen files abgelegt
+
+my $FOC_IMPORTDIR = 'daten-focus';                      #wo liegen die focus daten
+my $FOC_TABLENAME = 'focus_data';                       #in welche tabelle kommen die focus daten
