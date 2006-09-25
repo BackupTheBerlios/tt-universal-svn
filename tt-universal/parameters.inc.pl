7089e88907735ea3ea0a74a5d93694ad94781f11
rk-hannover <rk-hannover@e06bf135-151a-0410-b1a0-f07b4d66640d>
2006-09-25 21:36:11 +0000

    params aktualisiert

--- tt-universal/parameters.inc.pl
+++ tt-universal/parameters.inc.pl
@@ -1,11 +1,26 @@
-my $STAT_DB = 'spicers_stat'; #Name der Stat Datenbank
-my $STAT_DB_USER = 'root';    #Username für zugriff auf DB
-my $STAT_DB_PASS = '';        #passwort für zugriff auf DB
+use strict;
+use vars qw(
+$STAT_DB
+$STAT_DB_USER
+$STAT_DB_PASS
+$LOG_TABLE
+$STAT_STARTDIR
+$STAT_SAVEDIR
+$FOC_IMPORTDIR
+$FOC_TABLENAME
+);
 
-my $LOG_TABLE = 'import_log'; #name der logging tabelle
+$STAT_DB = 'spicers_stat'; #Name der Stat Datenbank
+$STAT_DB_USER = 'root';    #Username für zugriff auf DB
+$STAT_DB_PASS = '';        #passwort für zugriff auf DB
 
-my $STAT_STARTDIR = 'G:/win_data/projekte/spicers';     #wo liegen die daten der anwendung
-my $STAT_SAVEDIR = 'save';                              #wo werden die eingelesenen files abgelegt
+$LOG_TABLE = 'import_log'; #name der logging tabelle
 
-my $FOC_IMPORTDIR = 'daten-focus';                      #wo liegen die focus daten
-my $FOC_TABLENAME = 'focus_data';                       #in welche tabelle kommen die focus daten
+$STAT_STARTDIR = 'G:/win_data/projekte/spicers';     #wo liegen die daten der anwendung
+$STAT_SAVEDIR = 'save';                              #wo werden die eingelesenen files abgelegt
+
+$FOC_IMPORTDIR = 'daten-focus';                      #wo liegen die focus daten
+$FOC_TABLENAME = 'focus_data';                       #in welche tabelle kommen die focus daten
+
+
+1;
