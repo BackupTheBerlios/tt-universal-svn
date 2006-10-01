2e2858bdff1031fd029d36c2a06df69f7687c18e
rk-hannover <rk-hannover@e06bf135-151a-0410-b1a0-f07b4d66640d>
2006-10-01 12:07:36 +0000

    neuer db name, neuer pfad

--- tt-universal/parameters.inc.pl
+++ tt-universal/parameters.inc.pl
@@ -10,14 +10,15 @@ $FOC_IMPORTDIR
 $FOC_TABLENAME
 );
 
+$STAT_DB = 's_stat';       #Name der Stat Datenbank
 ##### $STAT_DB = 'spicers_stat'; #Name der Stat Datenbank
-$STAT_DB = 'spicers_stat'; #Name der Stat Datenbank
+
 $STAT_DB_USER = 'root';    #Username für zugriff auf DB
 $STAT_DB_PASS = '';        #passwort für zugriff auf DB
 
 $LOG_TABLE = 'import_log'; #name der logging tabelle
 
-$STAT_STARTDIR = 'G:/win_data/projekte/spicers';     #wo liegen die daten der anwendung
+$STAT_STARTDIR = '//gringots/netzdaten/win_data/projekte/spicers';     #wo liegen die daten der anwendung
 $STAT_SAVEDIR = 'save';                              #wo werden die eingelesenen files abgelegt
 
 $FOC_IMPORTDIR = 'daten-focus';                      #wo liegen die focus daten
