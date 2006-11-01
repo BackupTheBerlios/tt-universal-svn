e339a8eaebfc815ee1625581996d4106d6854f58
rk-hannover <rk-hannover@e06bf135-151a-0410-b1a0-f07b4d66640d>
2006-11-01 21:27:12 +0000

    zusatz params für gls, dhl und nightstar

--- tt-universal/parameters.inc.pl
+++ tt-universal/parameters.inc.pl
@@ -17,6 +17,14 @@ $FOC_IMPORTDIR
 $FOC_TABLENAME
 $LM1_IMPORTDIR
 $LM1_TABLENAME
+$DHL_EASY1_IMPORTDIR
+$DHL_EASY1_TABLENAME
+$GLS_GEP1_IMPORTDIR
+$GLS_GEP1_TABLENAME
+$NIGHT1_IN_IMPORTDIR
+$NIGHT1_IN_TABLENAME
+$NIGHT1_OUT_IMPORTDIR
+$NIGHT1_OUT_TABLENAME
 );
 
 
@@ -29,7 +37,8 @@ $STAT_DB_PASS = '';        #passwort für zugriff auf DB
 
 $LOG_TABLE = 'import_log'; #name der logging tabelle
 
-$STAT_STARTDIR = '//gringots/netzdaten/win_data/projekte/spicers';     #wo liegen die daten der anwendung
+$STAT_STARTDIR = 'e:/spicers';     #wo liegen die daten der anwendung
+# $STAT_STARTDIR = '//gringots/netzdaten/win_data/projekte/spicers';     #wo liegen die daten der anwendung
 $STAT_SAVEDIR = 'save';                              #wo werden die eingelesenen files abgelegt
 
 $FOC_IMPORTDIR = 'daten-focus';                      #wo liegen die focus daten
@@ -37,5 +46,18 @@ $FOC_TABLENAME = 'focus_data';                       #in welche tabelle kommen d
 
 $LM1_IMPORTDIR = 'lm1';                      #wo liegen die lm daten
 $LM1_TABLENAME = 'lm1_data';                       #in welche tabelle kommen die lm daten
+
+$DHL_EASY1_IMPORTDIR = 'dhl';                     #wo liegen die dhl easylog daten
+$DHL_EASY1_TABLENAME = 'dhl_easylog1';
+
+$GLS_GEP1_IMPORTDIR = 'gls';                      #wo liegen die gls gepard daten
+$GLS_GEP1_TABLENAME = 'gls_gepard1';
+
+$NIGHT1_IN_IMPORTDIR = 'nightstar/receive';       #wo liegen die nightstar daten
+$NIGHT1_IN_TABLENAME = 'nightstar1_in';
+
+$NIGHT1_OUT_IMPORTDIR = 'nightstar/send';         #wo liegen die nightstar daten
+$NIGHT1_OUT_TABLENAME = 'nightstar1_out';
+
 
 1;
