1b70db296e995d814a432bf6421d01255ac93c25
rk-hannover <rk-hannover@e06bf135-151a-0410-b1a0-f07b4d66640d>
2006-11-10 23:48:56 +0000

    daten holen dhl hauptprogramm

---
+++ tt-universal/get_dhl1.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl
+###########################################
+# get_dhl1.pl -- Import statistic from easylog into mysql tables
+# Robert Krauss, 2006 (rk-hannover@gmx.de)
+###########################################
+
+my $debug = 1;              # set to 1 for debug print (for each .pl .pm separately)
+
+use strict;
+use warnings;
+use DBI;
+use File::Copy;
+use Tt_global ();           #our own module for all relevant subroutines
+use Tt_parameters ();       #our own module for global vars
+
+sub get_dhl1();
+
+
+$| = 1;                     # don't buffer STDOUT (for EPIC use)
+
+#=============== MAIN =====================
+if ($debug) {print get_timestamp()," ScriptStart!\n"};
+get_dhl1();
+if ($debug) {print get_timestamp()," ScriptEnde!\n"};
+#=============== END MAIN =================
+
+
