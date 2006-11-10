5b91798236c520253abdd9b872b09ce35bbf9622
rk-hannover <rk-hannover@e06bf135-151a-0410-b1a0-f07b4d66640d>
2006-11-10 23:47:53 +0000

    änderungen

--- tt-universal/get_focus_lm.pl
+++ tt-universal/get_focus_lm.pl
@@ -4,6 +4,8 @@
 # Robert Krauss, 2006 (rk-hannover@gmx.de)
 ###########################################
 
+my $debug = 0;              # set to 1 for debug print (for each .pl .pm separately)
+
 use strict;
 use warnings;
 use DBI;
@@ -11,11 +13,13 @@ use File::Copy;
 use Tt_global ();           #our own module for all relevant subroutines
 use Tt_parameters ();       #our own module for global vars
 
-$| = 1;                     # don't buffer STDOUT
+
+
+$| = 1;                     # don't buffer STDOUT (for EPIC use)
 
 #=============== MAIN =====================
-print get_timestamp()," ScriptStart!\n";
+if ($debug) {print get_timestamp()," ScriptStart!\n"};
 get_focus();
 get_lm1();
-print get_timestamp()," Scriptende!\n";
+if ($debug) {print get_timestamp()," ScriptEnde!\n"};
 #=============== END MAIN =================
