c9fa48168230aa26e904cb7014faedb77977edd2
rk-hannover <rk-hannover@e06bf135-151a-0410-b1a0-f07b4d66640d>
2006-11-25 22:34:49 +0000

    neu: nightstar sent daten einlesen

---
+++ tt-universal/get_nstar1.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+###########################################
+# get_dhl1.pl -- Import statistic from easylog into mysql tables
+# Robert Krauss, 2006 (rk-hannover@gmx.de)
+###########################################
+
+my $debug = 1;              # set to 1 for debug print (for each .pl .pm separately)
+
+use strict;
+use warnings;
+use DBI;
+use File::Copy;
+use Tt_global ();           #our own module for all relevant subroutines
+use Tt_parameters ();       #our own module for global vars
+
+
+sub get_nightstar_send1($);
+
+
+$| = 1;                     # don't buffer STDOUT (for EPIC use)
+
+#=============== MAIN =====================
+if ($debug) {print get_timestamp()," ScriptStart!\n"};
+get_nightstar_send1('160');
+get_nightstar_send1('210');
+if ($debug) {print get_timestamp()," ScriptEnde!\n"};
+#=============== END MAIN =================
+
