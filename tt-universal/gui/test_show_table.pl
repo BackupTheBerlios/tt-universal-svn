47bb7e9cc8ebee973e918ba563c42e7291014c4d
rk-hannover <rk-hannover@e06bf135-151a-0410-b1a0-f07b4d66640d>
2006-12-31 00:12:02 +0000

    for testing

--- tt-universal/gui/test_show_table.pl
+++ tt-universal/gui/test_show_table.pl
@@ -1,6 +1,7 @@
 use strict;
-# use warnings;
+use warnings;
 use DBI;
+use CGI qw(:all);
 
 
 my $STAT_DB = 's_stat';        #Name der Stat Datenbank
@@ -11,18 +12,32 @@ my $STAT_DB_PASS = '';        #passwort für zugriff auf DB
 
 my $dbh = DBI->connect($DB_TYPE, $STAT_DB_USER, $STAT_DB_PASS, {RaiseError => 0}) or die "Database connection not made: $DBI::errstr";
 
-my $select1 = "select * from dhl_easylog1";
+my $select1 = "SELECT distinct cono, date_format(priodate,'%d.%m.%Y') as 'date' FROM focus_data
+where custno = '1603435' and status = '2' order by cono";
 my $sth = $dbh->prepare($select1);
 $sth->execute();
 
+my $q = CGI->new();
+print $q->header();
+print $q->start_html("Test1");
+print $q->h1( "Test Überschrift" );
+
 my @names = @{$sth->{NAME}};
 my $count = 1;
+my $var1;
+
+
 print "<table border=1>\n".join("",map{'<th>'.$_.'</th>'}@names)."\n";
 while(my $row = $sth->fetchrow_hashref()){
- $count++;
-  print "<tr>".join("~",map{'<td>'." bla".$_.'</td>'}@{$row}{@names})."</tr>\n";
-  if ($count == 5 ) {exit;}
+     $count++;
+     $var1 = "<tr>".join("",map{'<td>'.$_.'</td>'}@{$row}{@names})."</tr>\n";
+     $var1 =~ s!(.*)(<td>)(\d{6})(<\/td>)(.*)!$1$2<a href="http://www.domain.xy/script.pl?action=run;cono=$3" target="_blank">$3</a>$4$5!;
+     print $var1;
 #  for my $name(@names){
 #      push @{$hash{$name}},$row->{$name};
 #  }
 }
+
+
+print $q->end_html();
+print $q->redirect('http://1somewhere.else/in/movie.pl?x=1;y=2');
