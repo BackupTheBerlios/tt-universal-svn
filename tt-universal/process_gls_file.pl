9701560507e4deb24e3f98ab4c9d8ddb12b1d88b
rk-hannover <rk-hannover@e06bf135-151a-0410-b1a0-f07b4d66640d>
2007-01-19 15:56:27 +0000

    fehlerbeseitigung process_gls

--- tt-universal/process_gls_file.pl
+++ tt-universal/process_gls_file.pl
@@ -11,6 +11,7 @@ use warnings;
 use lib ('/spicers/scripts/perl');
 use lib ('D:/programme/EasyEclipse-LAMP102/workspace/tt-universal');
 use lib ('C:/Programme/EasyEclipse-LAMP102/workspace/tt-universal');
+use lib ('D:/EasyEclipseLAMP102/workspace/tt');
 use Tt_global;           #our own module for all relevant subroutines
 use Tt_parameters;       #our own module for global vars
 use DBI;
@@ -20,32 +21,32 @@ use Net::FTP;
 # http://www.tekromancer.com/perl2/12_1.html
 # http://www.schockwellenreiter.de/perl/ftp.html
 
-my $STAT_DB = 's_stat';        #Name der Stat Datenbank
-my $DB_HOST = 'localhost';		#Rechnername auf dem die MySQL DB liegt
-my $DB_TYPE = 'DBI:mysql:'.$STAT_DB.':'.$DB_HOST;	#DBI Zugriffsparameter für mysql
-
-my $STAT_DB_USER = 'root';    #Username für zugriff auf DB
-my $STAT_DB_PASS = '';        #passwort für zugriff auf DB
+#my $STAT_DB = 's_stat';        #Name der Stat Datenbank
+#my $DB_HOST = 'localhost';		#Rechnername auf dem die MySQL DB liegt
+#my $DB_TYPE = 'DBI:mysql:'.$STAT_DB.':'.$DB_HOST;	#DBI Zugriffsparameter für mysql
+#
+#my $STAT_DB_USER = 'root';    #Username für zugriff auf DB
+#my $STAT_DB_PASS = '';        #passwort für zugriff auf DB
 
 
 $| = 1;                     # don't buffer STDOUT (for EPIC use)
 
 #=============== MAIN =====================
 if ($debug) {print get_timestamp()," ScriptStart!\n"};
-# process_glsfile1('160');
-# process_glsfile1('210');
+process_glsfile1_read('160');
+# process_glsfile1_read('210');
 #if (send_ftp2server ("d:/down/kdpaket.dat","$GLS_FTPUSER160","$GLS_FTPPASS160","$GLS_FTPHOST160","$GLS_FTPPATH160")) {
 #     print "FTP hat geklappt\n";
 #}
 #else {
 #     print "FTP FEHLGESCHLAGEN!\n";
 #}
-if (get_fromftpserver ("kdpaket.dat","$GLS_FTPUSER160","$GLS_FTPPASS160","$GLS_FTPHOST160","$GLS_FTPPATH160","d:/down")) {
-     print "FTP holen hat geklappt\n";
-}
-else {
-     print "FTP holen FEHLGESCHLAGEN!\n";
-}
+#if (get_fromftpserver ("kdpaket.dat","$GLS_FTPUSER160","$GLS_FTPPASS160","$GLS_FTPHOST160","$GLS_FTPPATH160","d:/down")) {
+#     print "FTP holen hat geklappt\n";
+#}
+#else {
+#     print "FTP holen FEHLGESCHLAGEN!\n";
+#}
 
 if ($debug) {print get_timestamp()," ScriptEnde!\n"};
 #=============== END MAIN =================
