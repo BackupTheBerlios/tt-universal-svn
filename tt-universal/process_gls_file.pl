6260fc785e53f6b36c2b339b759fee2ae60cf9b1
rk-hannover <rk-hannover@e06bf135-151a-0410-b1a0-f07b4d66640d>
2007-02-04 21:00:22 +0000

    anpassungen rk

--- tt-universal/process_gls_file.pl
+++ tt-universal/process_gls_file.pl
@@ -20,41 +20,49 @@ use Net::FTP;
 
 my $timestamp;
 my $transfer_ok = 0;
+my $transferfile;             #file with path to send
 
 
 $| = 1;                     # don't buffer STDOUT (for EPIC use)
 
 #=============== MAIN =====================
 if ($debug) {print get_timestamp()," ScriptStart!\n"};
-# $timestamp = process_glsfile1_read('160');        # kdpaket.dat für lager 160 einlesen
-# if ($timestamp gt 0 ) {                           # wenn was eingelesen wurde, auf doppelte aussendung prüfen
-# $timestamp = '20070121132619'; #  20070120222752
-$timestamp = '20070120222752';
+$timestamp = process_glsfile1_read('160');        # kdpaket.dat für lager 160 einlesen
+if ($timestamp gt 0 ) {                           # wenn was eingelesen wurde, auf doppelte aussendung prüfen
+	comp_p_out_gepart($timestamp);                #mit gepart vergleichen
+	comp_p_out_dhl_easylog($timestamp);           #mit easylog vergleichen
+	comp_p_out_nightstar($timestamp);             #mit nightstar vergleichen
+	$transfer_ok = writefile_p_out($timestamp,'160');
+	if ($transfer_ok eq '1') {                 # wenn ein gültiger wert zurückgegeben wird...
+         print "Transfer OK. Retvalue = $transfer_ok\n";
+         update_p_out_sent_ok($timestamp,'160');  #versand der kdpaket.dat in der tabelle vermerken
+    }
+    else {
+         print "Transfer 160 NOT OK? Retvalue = $transfer_ok\n";
+    }
+}
+else {
+    print "160. Kein Timestamp. Nichts eingelesen.\n";
+
+}
+
+$timestamp = process_glsfile1_read('210');        # kdpaket.dat für lager 210 einlesen
+if ($timestamp gt 0 ) {                           # wenn was eingelesen wurde, auf doppelte aussendung prüfen
 	comp_p_out_gepart($timestamp);
 	comp_p_out_dhl_easylog($timestamp);
 	comp_p_out_nightstar($timestamp);
-	writefile_p_out($timestamp,'160');
-# }
-#$timestamp = process_glsfile1_read('210');        # kdpaket.dat für lager 210 einlesen
-#if ($timestamp gt 0 ) {                           # wenn was eingelesen wurde, auf doppelte aussendung prüfen
-#	comp_p_out_gepart($timestamp);
-#	comp_p_out_dhl_easylog($timestamp);
-#	comp_p_out_nightstar($timestamp);
-#}
-
-# process_glsfile1_read('210');
-#if (send_ftp2server ("d:/down/kdpaket.dat","$GLS_FTPUSER160","$GLS_FTPPASS160","$GLS_FTPHOST160","$GLS_FTPPATH160")) {
-#     print "FTP hat geklappt\n";
-#}
-#else {
-#     print "FTP FEHLGESCHLAGEN!\n";
-#}
-#if (get_fromftpserver ("kdpaket.dat","$GLS_FTPUSER160","$GLS_FTPPASS160","$GLS_FTPHOST160","$GLS_FTPPATH160","d:/down")) {
-#     print "FTP holen hat geklappt\n";
-#}
-#else {
-#     print "FTP holen FEHLGESCHLAGEN!\n";
-#}
+	$transfer_ok = writefile_p_out($timestamp,'210');
+	if ($transfer_ok eq '2') {                 # wenn ein gültiger wert zurückgegeben wird...
+         print "Transfer OK. Retvalue = $transfer_ok\n";
+         update_p_out_sent_ok($timestamp,'210');
+    }
+    else {
+         print "Transfer 210 NOT OK? Retvalue = $transfer_ok\n";
+    }
+}
+else {
+    print "210. Kein Timestamp. Nichts eingelesen.\n";
+}
 
 if ($debug) {print get_timestamp()," ScriptEnde!\n"};
 #=============== END MAIN =================
